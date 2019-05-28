10f474198df5e02da5ecd11eedc535bd359a3d59
Benjamin Willems <bw@benjaminwil.info>
2019-05-28 08:26:33 -0700

    Get int value from a Roman number
    There are two things going on here:
    
      1. Numbers are calculated. XI => 10 + 1.
      2. Numbers are checked for "subtractive notation" (like IX being "one
         less than 10"), and made into negative integers to match.
    
    Once we have the correct (positive or negative) value of each integer,
    we get the Roman number value:
    
        IX => [-1, 10]
        XI => [10, 1]

--- lib/roman_numeral_calc/number.rb
+++ lib/roman_numeral_calc/number.rb
@@ -8,6 +8,23 @@ module RomanNumeralCalc
       @symbols = symbols
     end
 
+    def evaluate
+      numeric_values.sum
+    end
+
+    private
+
+    def numeric_values
+      symbol_values.map.with_index do |value, i|
+        next_value = symbol_values[i+1]
+        if next_value
+          value < next_value  ? -(value) : value
+        else
+          value
+        end
+      end
+    end
+
     def symbols
       {
         :I => 1,
@@ -17,5 +34,8 @@ module RomanNumeralCalc
       }
     end
 
+    def symbol_values
+      @input.chars.map { |char| @symbols[char.upcase.to_sym] }
+    end
   end
 end
