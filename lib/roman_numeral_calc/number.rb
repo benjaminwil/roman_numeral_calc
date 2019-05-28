726ee94264621121f1b79f57fc66348159b763a1
Benjamin Willems <bw@benjaminwil.info>
2019-05-28 08:23:04 -0700

    Add base Roman symbols to Number class

--- lib/roman_numeral_calc/number.rb
+++ lib/roman_numeral_calc/number.rb
@@ -1,4 +1,21 @@
 module RomanNumeralCalc
   class Number
+
+    attr_reader :symbols
+
+    def initialize(user_input)
+      @input = user_input
+      @symbols = symbols
+    end
+
+    def symbols
+      {
+        :I => 1,
+        :V => 5,
+        :X => 10,
+        :L => 50
+      }
+    end
+
   end
 end
