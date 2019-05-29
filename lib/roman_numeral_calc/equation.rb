143bc08bd63b439a8144925d21ed441b427184b3
Benjamin Willems <bw@benjaminwil.info>
2019-05-28 19:49:19 -0700

    Test and write Equation class. Yeah.

--- lib/roman_numeral_calc/equation.rb
+++ lib/roman_numeral_calc/equation.rb
@@ -1,4 +1,36 @@
 module RomanNumeralCalc
   class Equation
+
+    attr_reader :result
+
+    def initialize(operand1, operand2, operator)
+      @input = {
+        :operand1 => operand1,
+        :operand2 => operand2,
+        :operator => operator
+      }
+      @output = evaluate
+    end
+
+    private
+
+    def evaluate
+      operand1 = @input[:operand1]
+      operator = @input[:operator]
+      operand2 = @input[:operand2]
+
+      case operator
+      when :+
+        operand1 + operand2
+      when :/
+        operand1 / operand2
+      when :*
+        operand1 * operand2
+      when :-
+        operand1 - operand2
+      else
+        "error"
+      end
+    end
   end
 end
--- test/roman_numeral_calc/equation_test.rb
+++ test/roman_numeral_calc/equation_test.rb
@@ -4,11 +4,11 @@ module RomanNumeralCalc
   class EquationTest < Minitest::Test
 
     def test_that_two_numbers_can_be_added
-      operand1 = Operand.new('iv')
-      operator = Operator.new(' + ')
-      operand2 = Operand.new('vi')
+      operand1 = Operand.new('iv').output
+      operator = Operator.new(' + ').output
+      operand2 = Operand.new('vi').output
       equation = Equation.new(operand1, operand2, operator)
-      assert_equal 10, equation.result
+      assert_equal 10, equation.output
     end
 
   end
