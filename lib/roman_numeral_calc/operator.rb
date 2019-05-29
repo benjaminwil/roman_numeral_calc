09a1d7ba1a42750148dea6c54caef7e8ac8c0e47
Benjamin Willems <bw@benjaminwil.info>
2019-05-28 18:56:51 -0700

    Add simple Operator validation

--- lib/roman_numeral_calc/operator.rb
+++ lib/roman_numeral_calc/operator.rb
@@ -5,8 +5,23 @@ module RomanNumeralCalc
     
     include ::RomanNumeralCalc::Operators
     
+    attr_reader :input, :valid
+
     def initialize(user_input)
       @input = user_input
+      valid_operator? ? @valid = true : @valid = false
+    end
+
+    private
+
+    def valid_operator?
+      check_for_operators.select { |state| state == true }.count == 1
+    end
+
+    def check_for_operators
+     operators.map do |operator, regex|
+       @input.match?(regex)
+     end
     end
 
   end
